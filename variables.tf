c7c531ede7bd52a5b315453a171928400817e35d
naeemhdx <77334662+naeemhdx@users.noreply.github.com>
2023-05-28 23:12:40 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+#authentication variable
+variable "DATABRICKS_HOST" {}
+variable "DATABRICKS_TOKEN" {}
+#Basic Details
+variable "cluster-name" {}
+variable "spark-version" {
+  type        = string
+  default     = "10.4.x-scala2.12"
+  description = "Runtime version of the cluster. Any supported databricks_spark_version id."
+}
+
+#ADLS SPN Access details
+# variable "adls_name" {}
+# variable "scope_name" {}
+# variable "clientID_key" {}
+# variable "secret_key" {}
+#Metastore details
+# variable "presto_ip" {}
+# variable "presto_port" {}
+#Node Type
+variable "compute" {}
+
+# variable "minworker" {}
+# variable "maxworker" {}
