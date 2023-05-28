f9ddb08aab6ef01e84eed3386268a4ed535c3a7c
naeemhdx <77334662+naeemhdx@users.noreply.github.com>
2023-05-28 23:07:21 +0530

    Create auth.tf
    databricks cluster auth and provider

---
+++ auth.tf
@@ -0,0 +1,17 @@
+#Darabricks Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    databricks = {
+      source = "databricks/databricks"
+    }
+  }
+}
+
+# Use Azure CLI authentication.
+provider "databricks" {
+  host = var.DATABRICKS_HOST
+  token = var.DATABRICKS_TOKEN
+}
